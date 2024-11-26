a3747fd2c3f611d61f58a2ad842cbf7b97b62ae7
kdidrees <Khurshid@qurilo.com>
2024-11-26 16:27:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import AutoRepair from "./components/AboutUs/AutoRepair";
 import Benefits from "./components/AboutUs/Benefits";
 import Leader from "./components/AboutUs/Leader";
+import Satisfaction from "./components/AboutUs/Satisfaction";
 import Faq from "./components/Faq";
 import Testimonial from "./components/Testimonial";
 
 function App() {
   return (
-    <div>
-      <AutoRepair />
-    </div>
+
+      <Satisfaction/>
+    
   );
 }
 
