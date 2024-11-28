a102c9b7b523dd8642e60d3aa197d9ac3aaeef9e
kdidrees <Khurshid@qurilo.com>
2024-11-28 15:59:17 +0530

    update

--- src/components/steamWash/SteamWash.jsx
+++ src/components/steamWash/SteamWash.jsx
@@ -25,7 +25,7 @@ export default function SteamWash() {
   const rightList = listData.slice(7);
 
   return (
-    <div className="grid lg:grid-cols-3 lg:px-20 grid-cols-1 items-stretch gap-4 py-10">
+    <div className="grid lg:grid-cols-3 lg:px-20 px-4 grid-cols-1 items-stretch gap-4 py-10">
       {/* Left image */}
       <div className="col-span-1 h-full bg-red-600">
         <img
@@ -36,24 +36,17 @@ export default function SteamWash() {
       </div>
 
       {/* List items */}
-      <div className="col-span-2 h-full grid lg:grid-cols-2 xl:grid-cols-2 items-center gap-2">
+      <div className="lg:col-span-2 h-full grid lg:grid-cols-2 grid-cols-1 items-center ">
         {/* Heading */}
         <div className="col-span-3">
-          <h2 className="text-xl font-bold text-primary  text-left">
+          <h2 className="text-xl font-bold text-primary  lg:text-left text-center">
             FEATURES
           </h2>
         </div>
 
-        {/* Left list */}
-        <ul>
-          {leftList.map((item, index) => (
-            <ListItem key={item.id} text={item.text} isFirst={index === 0} />
-          ))}
-        </ul>
-
-        {/* Right list */}
-        <ul>
-          {rightList.map((item, index) => (
+        {/* Unified list for smaller screens */}
+        <ul className="col-span-2 lg:grid lg:grid-cols-2 grid-cols-1 ">
+          {listData.map((item, index) => (
             <ListItem key={item.id} text={item.text} isFirst={index === 0} />
           ))}
         </ul>
@@ -65,11 +58,9 @@ export default function SteamWash() {
 function ListItem({ text, isFirst }) {
   return (
     <li
-      className={`text-body-color dark:text-dark-6 flex text-base ${
-        isFirst ? "mt-0" : "mt-4"
-      } mb-2`}
+      className={`text-body-color dark:text-dark-6 flex lg:justify-start justify-center  text-base mt-2 mb-2 `}
     >
-      <span className="bg-primary mr-2.5 flex h-[26px] w-[26px] items-center justify-center rounded-full text-base text-white">
+      <span className="bg-primary mr-2.5 flex h-[26px] lg:w-[26px] items-center justify-center rounded-full text-base text-white">
         <img src={point_img} alt="Point" />
       </span>
       {text}
